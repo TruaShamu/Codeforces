c028a9c9d24073feb7fce1268ba90fcfae65e5d9
Trua Shamu <48630514+TruaShamu@users.noreply.github.com>
2021-04-26 17:28:35 -0500

    Create cf280a.java

---
+++ cf280a.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class cf280a {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int totalCubes = sc.nextInt();
+        int totalUsed = 0;
+        for (int height = 0; ; height++) {
+            int rowCubes = ((height) * (height + 1)) / 2;
+            if (totalUsed + rowCubes > totalCubes) {
+                System.out.println(height - 1);
+                System.exit(0);
+            }
+            if (totalUsed + rowCubes == totalCubes) {
+                System.out.println(height);
+                System.exit(0);
+            }
+            totalUsed = totalUsed + rowCubes;
+        }
+ 
+    }
+}
