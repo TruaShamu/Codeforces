c63730797ecd753e16a06b12f6b68cd855aefd38
Trua Shamu <48630514+TruaShamu@users.noreply.github.com>
2021-04-26 16:39:34 -0500

    Update and rename 1A.java to cf1a.java

--- cf1a.java
+++ cf1a.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class cf1a {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long width = sc.nextLong();
