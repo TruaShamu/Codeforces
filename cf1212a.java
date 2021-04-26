75d0b97f3dca825a3e9267a0f4adca31cc5499ea
Trua Shamu <48630514+TruaShamu@users.noreply.github.com>
2021-04-26 17:00:46 -0500

    Create cf1212a.java

---
+++ cf1212a.java
@@ -0,0 +1,21 @@
+//This is probably broken
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class cf1212a {
+    public static void main(String[] args) {
+        Scanner inputReader = new Scanner(System.in);
+        int cases = inputReader.nextInt();
+        for (int i = 0; i < cases; i++) {
+            int students = 2 * inputReader.nextInt();
+            int[] array = new int[students];
+            for (int j = 0; j < students; j++) {
+                array[j] = inputReader.nextInt();
+            }
+            Arrays.sort(array);
+            System.out.println(array[students / 2] - array[students / 2 - 1]);
+        }
+
+
+    }
+}
