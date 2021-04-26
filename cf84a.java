5a89989d491f2fbcf6b8ec1c788f688630502a52
Trua Shamu <48630514+TruaShamu@users.noreply.github.com>
2021-04-26 16:56:15 -0500

    Create cf84a.java

---
+++ cf84a.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+ 
+public class cf84a {
+    public static void main(String[] args) {
+        Scanner inputReader = new Scanner(System.in);
+        int n = Integer.parseInt(inputReader.nextLine());
+        System.out.println(3* (n/2));
+    }
+}
